c23cb08e2923018e42150dcf3da286ddc8b54583
rahima <129212106+rnue6@users.noreply.github.com>
2023-04-17 14:08:46 -0400

    part 2

--- benofrd.py
+++ benofrd.py
@@ -4,17 +4,24 @@ folder = os.getcwd()
 fileName = folder + "\\sales.csv"
 file = open(fileName, 'r')
 salesList = []
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def readSalesFile():
     readFile = file.readlines()[1:]
     for line in readFile:
         line.replace("\r", '')
-        hi = line.split(',')
-        hello = hi[1]
-        print(hello)
+        split = line.split(',')
+        firstNum = split[1]
+        salesList.append(int(firstNum[0]))
+
+def numFrequency(num, list):
+    for num in list:
+        this = (list.count(num))
+        percentage = this/len(list)*100
 
 if num == "1":
     readSalesFile()
+    numFrequency(numList, salesList)
     file.close()
 
 else:
