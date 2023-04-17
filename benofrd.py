9959e923466e8301ca946e0c38f519050b92a2c7
rahima <129212106+rnue6@users.noreply.github.com>
2023-04-17 13:11:38 -0400

    step 1

--- benofrd.py
+++ benofrd.py
@@ -1,3 +1,22 @@
-print("Hello World")
+num = input("Press one to read the sales file, press three to check for accounting fraud. ")
+import os
+folder = os.getcwd()
+fileName = folder + "\\sales.csv"
+file = open(fileName, 'r')
+salesList = []
 
-# test
+def readSalesFile():
+    readFile = file.readlines()[1:]
+    for line in readFile:
+        line.replace("\r", '')
+        hi = line.split(',')
+        hello = hi[1]
+        print(hello)
+
+if num == "1":
+    readSalesFile()
+    file.close()
+
+else:
+    file.close()
+    print("end")
